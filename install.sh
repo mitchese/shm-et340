a3b57816f6c42ec7f5b346e364b3d99c5f97b65c
christian1980nrw <christian1980nrw@gmail.com>
2023-01-07 17:28:12 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,12 +3,11 @@
 mkdir /data/drivers/shm-et340
 mkdir /data/drivers/shm-et340/service
 cd /data/drivers/shm-et340
-wget https://github.com/christian1980nrw/shm-et340-fixed-vrm-portal/raw/master/shm-et340
+wget https://github.com/mitchese/shm-et340/releases/download/v0.3/shm-et340
 chmod +x ./shm-et340
 cd /data/drivers/shm-et340/service
 wget https://raw.githubusercontent.com/christian1980nrw/shm-et340-fixed-vrm-portal/master/service/run
-chmod +x ./shm-et340-service.sh
-mv ./shm-et340-service.sh ./run
+chmod +x ./run
 ln -s  /data/drivers/shm-et340/service /service/shm-et340
 echo >> /data/rc.local
 echo "ln -s /data/drivers/shm-et340/service /service/shm-et340" >> /data/rc.local
